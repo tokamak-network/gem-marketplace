5b4c8f6e7c2d84a40540648a0676b10106e11fa9
jusdy <pauleta0126@gmail.com>
2024-06-05 22:38:09 +0900

    feat: nft card ui

--- src/components/market/GemCard.tsx
+++ src/components/market/GemCard.tsx
@@ -1,8 +1,67 @@
-const GemCard = () => {
-  return (
-    <>
-    </>
-  )
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import PriceContainer from "../common/PriceContainer";
+import HighArrow from "@/assets/icon/higharrow.svg";
+import Image from "next/image";
+import SavedIcon from "../common/SavedIcon";
+
+interface GemCardType {
+  rarity: string;
+  rarityScore: number;
+  staked: number;
+  dailyChange: number;
 }
+
+const GemCard = ({ rarity, rarityScore, staked, dailyChange }: GemCardType) => {
+  const [isSaved, setSaved] = useState<boolean>(false);
+
+  return (
+    <Flex
+      w={212}
+      h={272}
+      pos={"relative"}
+      flexDir={"column"}
+      rounded={8}
+      overflow={"hidden"}
+    >
+      <Box
+        pos={"absolute"}
+        top={"10px"}
+        right={"10px"}
+        cursor={"pointer"}
+        zIndex={10}
+        onClick={() => setSaved((prev) => !prev)}
+      >
+        <SavedIcon isFill={isSaved} />
+      </Box>
+
+      <Box w={"full"} h={"full"} bg={"white"} opacity={0.05}></Box>
+      <Flex
+        w={"full"}
+        h={53}
+        bgColor={"#00000080"}
+        justify={"space-between"}
+        p={"10px"}
+      >
+        <Flex flexDir={"column"} justify={"space-between"}>
+          <Text fontSize={14} fontWeight={600}>
+            {rarity} {rarityScore}%
+          </Text>
+          <Flex columnGap={1} align={"center"}>
+            <Text fontSize={10} fontWeight={400}>
+              Staked ${staked}{" "}
+            </Text>
+            <Image alt="arrow" src={HighArrow} />
+            <Text color={"#61FF00"} fontSize={10} fontWeight={400}>
+              {dailyChange}%
+            </Text>
+          </Flex>
+        </Flex>
+
+        <PriceContainer price={100} />
+      </Flex>
+    </Flex>
+  );
+};
 
 export default GemCard;
--- src/pages/market/index.tsx
+++ src/pages/market/index.tsx
@@ -3,8 +3,9 @@ import RarityList from "@/components/market/RarityList";
 import ColorList from "@/components/market/ColorList";
 import PriceContainer from "@/components/common/PriceContainer";
 
-import GEM from "@/assets/images/sample_gem.png"
+import GEM from "@/assets/images/sample_gem.png";
 import Image from "next/image";
+import GemCard from "@/components/market/GemCard";
 
 const MarketPage = () => {
   const theme = useTheme();
@@ -15,25 +16,93 @@ const MarketPage = () => {
       <ColorList />
 
       <Flex columnGap={4} mt={73}>
-        <Flex pos={"relative"} w={212} h={272} bgImage={"/image/gempack.png"} flexDir={"column"} justify={"end"}>
-          <Box pos={"absolute"} transform={"translateX(-50%)"} left={"calc(50%)"} top={"-70px"} width={200} height={200}>
-            <Image alt="gem" src={GEM}/>
+        <Flex
+          pos={"relative"}
+          w={212}
+          h={272}
+          bgImage={"/image/gempack.png"}
+          flexDir={"column"}
+          justify={"end"}
+        >
+          <Box
+            pos={"absolute"}
+            transform={"translateX(-50%)"}
+            left={"calc(50%)"}
+            top={"-70px"}
+            width={200}
+            height={200}
+          >
+            <Image alt="gem" src={GEM} />
           </Box>
 
-          <Text fontSize={24} fontWeight={700} px={17}>GEM PACK</Text>
-          <Text fontFamily={theme.fonts.Inter} fontSize={14} fontWeight={400} px={17} mt={4} mb={5}>Obtain 1 Gem ranging from Base up to Legendary!</Text>
+          <Text fontSize={24} fontWeight={700} px={17}>
+            GEM PACK
+          </Text>
+          <Text
+            fontFamily={theme.fonts.Inter}
+            fontSize={14}
+            fontWeight={400}
+            px={17}
+            mt={4}
+            mb={5}
+          >
+            Obtain 1 Gem ranging from Base up to Legendary!
+          </Text>
 
-          <Flex w={"full"} h={53} bg={"#00000080"} justify={"end"} px={"9px"} align={"center"}>
-            <PriceContainer price={10}/>
+          <Flex
+            w={"full"}
+            h={53}
+            bg={"#00000080"}
+            justify={"end"}
+            px={"9px"}
+            align={"center"}
+          >
+            <PriceContainer price={10} />
           </Flex>
         </Flex>
 
-        <Flex pos={"relative"} w={668} h={272} flexDir={"column"} pt={"30px"} pb={"20px"} px={"30px"} >
+        <Flex
+          pos={"relative"}
+          w={668}
+          h={272}
+          flexDir={"column"}
+          pt={"30px"}
+          pb={"20px"}
+          px={"30px"}
+        >
           <Box pos={"absolute"} top={0} left={0}>
-            <Image alt="halloween" width={668} height={272} src={"/image/halloween.png"}></Image>
+            <Image
+              alt="halloween"
+              width={668}
+              height={272}
+              src={"/image/halloween.png"}
+            ></Image>
           </Box>
-          <Box pos={"absolute"} top={0} left={0} zIndex={10} bgGradient={"radial-gradient(#00000000, #000000D9)"} w={"full"} h={272}></Box>
+          <Box
+            pos={"absolute"}
+            top={0}
+            left={0}
+            zIndex={10}
+            bgGradient={"radial-gradient(#00000000, #000000D9)"}
+            w={"full"}
+            h={272}
+          ></Box>
         </Flex>
+      </Flex>
+
+      <Flex mt={4} gap={4} flexWrap={"wrap"}>
+        {Array(20)
+          .fill("")
+          .map((item) => {
+            return (
+              <GemCard
+                rarity="Mythic"
+                rarityScore={1}
+                staked={253.2}
+                dailyChange={16.7}
+              />
+            );
+          })}
       </Flex>
     </Box>
   );
