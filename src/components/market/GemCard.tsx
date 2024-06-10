bbb5e7370e6887008d7833a113e74faed3c94233
jusdy <pauleta0126@gmail.com>
2024-06-10 16:10:39 +0900

    feat: flip effect on nft card

--- src/components/market/GemCard.tsx
+++ src/components/market/GemCard.tsx
@@ -14,53 +14,88 @@ interface GemCardType {
 
 const GemCard = ({ rarity, rarityScore, staked, dailyChange }: GemCardType) => {
   const [isSaved, setSaved] = useState<boolean>(false);
+  const [isHover, setHover] = useState<boolean>(false);
 
   return (
-    <Flex
+    <Box
       w={212}
       h={272}
-      pos={"relative"}
-      flexDir={"column"}
-      rounded={8}
-      overflow={"hidden"}
+      bgColor={"transparent"}
+      style={{ perspective: "1000px" }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
       <Box
-        pos={"absolute"}
-        top={"10px"}
-        right={"10px"}
-        cursor={"pointer"}
-        zIndex={10}
-        onClick={() => setSaved((prev) => !prev)}
+        w={"100%"}
+        h={"100%"}
+        transition={"transform 0.8s"}
+        style={{
+          transformStyle: "preserve-3d",
+          transform: isHover ? "rotateY(180deg)" : "",
+        }}
+        pos={"relative"}
       >
-        <SavedIcon isFill={isSaved} />
-      </Box>
+        <Flex
+          pos={"absolute"}
+          w={"100%"}
+          h={"100%"}
+          flexDir={"column"}
+          rounded={8}
+          overflow={"hidden"}
+          style={{
+            backfaceVisibility: "hidden",
+          }}
+        >
+          <Box
+            pos={"absolute"}
+            top={"10px"}
+            right={"10px"}
+            cursor={"pointer"}
+            zIndex={10}
+            onClick={() => setSaved((prev) => !prev)}
+          >
+            <SavedIcon isFill={isSaved} />
+          </Box>
 
-      <Box w={"full"} h={"full"} bg={"white"} opacity={0.05}></Box>
-      <Flex
-        w={"full"}
-        h={53}
-        bgColor={"#00000080"}
-        justify={"space-between"}
-        p={"10px"}
-      >
-        <Flex flexDir={"column"} justify={"space-between"}>
-          <Text fontSize={14} fontWeight={600}>
-            {rarity} {rarityScore}%
-          </Text>
-          <Flex columnGap={1} align={"center"}>
-            <Text fontSize={10} fontWeight={400}>
-              Staked ${staked}{" "}
-            </Text>
-            <Image alt="arrow" src={HighArrow} />
-            <Text color={"#61FF00"} fontSize={10} fontWeight={400}>
-              {dailyChange}%
-            </Text>
+          <Box w={"full"} h={"full"} bg={"white"} opacity={0.05}></Box>
+          <Flex
+            w={"full"}
+            h={53}
+            bgColor={"#00000080"}
+            justify={"space-between"}
+            p={"10px"}
+          >
+            <Flex flexDir={"column"} justify={"space-between"}>
+              <Text fontSize={14} fontWeight={600}>
+                {rarity} {rarityScore}%
+              </Text>
+              <Flex columnGap={1} align={"center"}>
+                <Text fontSize={10} fontWeight={400}>
+                  Staked ${staked}{" "}
+                </Text>
+                <Image alt="arrow" src={HighArrow} />
+                <Text color={"#61FF00"} fontSize={10} fontWeight={400}>
+                  {dailyChange}%
+                </Text>
+              </Flex>
+            </Flex>
+
+            <PriceContainer price={100} />
           </Flex>
         </Flex>
 
-        <PriceContainer price={100} />
-      </Flex>
-    </Flex>
+        <Box
+          pos={"absolute"}
+          bgColor={"blue"}
+          w={"100%"}
+          h={"100%"}
+          style={{
+            backfaceVisibility: "hidden",
+            transform: "rotateY(180deg)"
+          }}
+        ></Box>
+      </Box>
+    </Box>
   );
 };
 
