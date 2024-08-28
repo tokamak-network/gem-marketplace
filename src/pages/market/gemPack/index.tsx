1fd4aaf46a79c8f5674d6b236582f1fec18e822f
jusdy <pauleta0126@gmail.com>
2024-08-28 10:34:40 +0900

    feat: add new details for gem item

--- src/pages/market/gemPack/index.tsx
+++ src/pages/market/gemPack/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useAccount } from "wagmi";
-import { Box, Button, Flex, Text, useTheme } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Text, useTheme } from "@chakra-ui/react";
 
 import { GemList } from "@/constants";
 import { CardType, GemStandard } from "@/types";
@@ -91,7 +91,33 @@ const GemPack = ({ id, mode }: ItemProps) => {
       </Flex>
 
       <Flex w={"100%"} mt={10} columnGap={6} flexGrow={1} h={"fit-content"}>
-        <Box w={"100%"} h={"100%"} bgColor={"#191A22"} rounded={16} />
+        <Box w={"100%"} maxH={"250px"} bgColor={"#191A22"} rounded={16} p={"36px"} overflow={"auto"}>
+          <Text fontWeight={600} fontSize={18}>Details</Text>
+
+          <Flex mt={6} flexDir={"column"} rowGap={4} fontSize={14} fontWeight={300}>
+            <Flex>
+              <Text minW={"135px"} color={"#FFFFFF80"}>Amount Minted:</Text>
+              <Text>1</Text>
+            </Flex>
+            <Flex>
+              <Text minW={"135px"} color={"#FFFFFF80"}>Amount Minted:</Text>
+              <Text color={"#0380FF"}>0x0134afd600000000000002386f26fc10000000000000000000119</Text>
+            </Flex>
+            <Flex>
+              <Text minW={"135px"} color={"#FFFFFF80"}>Amount Minted:</Text>
+              <Text color={"#0380FF"}>0x88f8Dbd3dC44c6E2368258D3eee8EB907AF191</Text>
+            </Flex>
+            <Flex>
+              <Text minW={"135px"} color={"#FFFFFF80"}>Amount Minted:</Text>
+              <Text color={"#0380FF"}>0x1cACC96e5F01e2849E6036F25531A9A064D2FB5f</Text>
+            </Flex>
+            <Flex>
+              <Text minW={"135px"} color={"#FFFFFF80"}>Amount Minted:</Text>
+              <Text>ERC-721</Text>
+            </Flex>
+          </Flex>
+        </Box>
+
         <Box w={"100%"} h={"100%"} bgColor={"#191A22"} rounded={16} />
       </Flex>
     </Flex>
