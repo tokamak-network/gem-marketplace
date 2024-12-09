f71e78fb742214908c827c38e0c27a89035f2c11
jusdy <pauleta0126@gmail.com>
2024-12-09 16:49:11 +0900

    fix: add gtg script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const GoogleAnalyticsScript = () => {
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-RSTR92STYJ"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RSTR92STYJ');`}
+      </Script>
+    </>
+  );
+};
 
 export default function Document() {
   return (
     <Html lang="en">
+      <GoogleAnalyticsScript />
       <Head />
       <title>GemSTON MVP</title>
       <body>
