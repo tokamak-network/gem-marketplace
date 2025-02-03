c677426398a9dcafb924478d5a4eb8e4c95eb3c0
jusdy <pauleta0126@gmail.com>
2025-02-03 22:10:06 +0900

    fix: spinner middle placement

--- src/pages/market/index.tsx
+++ src/pages/market/index.tsx
@@ -125,13 +125,15 @@ const MarketPage = () => {
         }
         hasMore={hasMore}
         loader={
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="lg"
-          />
+          <Center w={"100%"} overflow={"hidden"}>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="lg"
+            />
+          </Center>
         }
       >
         <Flex gap={4} flexWrap={"wrap"} mt={73}>
